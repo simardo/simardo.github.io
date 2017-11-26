7ef2f9f162f95238eb3314d269f2495f4c7318a5
simardo <simardo@gmail.com>
2017-11-26 17:06:58 -0500

    update with math repo

--- README.md
+++
@@ -1,1 +0,0 @@
-# simardo.github.io
---
+++ components/welcome/welcome.d.ts
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+export default class Welcome extends Vue {
+    private max;
+    private max1;
+    private max2;
+    private first;
+    private second;
+    private operator;
+    private answer;
+    private correction;
+    protected mounted(): void;
+    private doNext();
+    private onSubmit();
+}
