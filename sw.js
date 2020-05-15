40446b6303c2bdc4d7c79a4084a05ad4eda7a6e7
Martin Simard <martin.simard@dti.ulaval.ca>
2020-05-15 11:14:54 -0400

    test carte virtuelle

---
+++ sw.js
@@ -0,0 +1,33 @@
+(async function () {
+    self.addEventListener('install', e => {
+        console.log('installed');
+
+        caches.open('my-cache').then(c => {
+            c.addAll(['/carte']);
+        });
+    });
+
+    self.addEventListener('fetch', event => {
+        console.log('fetch', event.request);
+
+        event.respondWith(
+            caches.match(event.request).then(function (response) {
+                return response || fetch(event.request);
+            })
+        );
+
+
+        // if (event.request.method === 'GET' &&
+        //     event.request.headers.get('accept').indexOf('text/html') !== -1) {
+        //     console.log('Handling fetch event for', event.request.url);
+        //     event.respondWith(
+        //         fetch(event.request).catch(function (e) {
+        //             console.error('Fetch failed; returning offline page instead.', e);
+        //             // return caches.open(OFFLINE_CACHE).then(function (cache) {
+        //             //     return cache.match(OFFLINE_URL);
+        //             // });
+        //         })
+        //     );
+        // }
+    });
+})();
